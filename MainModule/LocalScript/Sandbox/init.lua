9bfbfff3888d20770572915c2862944930ce449d
interpreterK <80614657+interpreterK@users.noreply.github.com>
2022-05-24 09:18:34 -0400

    Update progress

--- MainModule/LocalScript/Sandbox/init.lua
+++ MainModule/LocalScript/Sandbox/init.lua
@@ -10,13 +10,13 @@ return function ()
 	local Player = Players.LocalPlayer
 	local IsTheReplicator = Player.Name == fakeCharacter.Name
 
-	local real = {game = game}
+	local real = {game = game, workspace = workspace}
 	local function fakeEvent()
-		local Bind = Instance.new("BindableEvent")
-		return Bind.Event
+		return Instance.new("BindableEvent").Event
 	end
 	local function newObject(real)
 		local Object = {}
+		Object.__metatable = "This metatable is locked"
 		Object.__index = function(self,ind)
 			return rawget(self,ind) or real[ind]
 		end
@@ -27,22 +27,47 @@ return function ()
 			return real.Name
 		end
 	end
-	local MouseEvents = {"Button1Down","Button1Up","Button2Down","Button2Up","KeyDown","KeyUp"}
+	local Mouse, MouseEvents = {}, {"Button1Down","Button1Up","Button2Down","Button2Up","KeyDown","KeyUp"}
 	if IsTheReplicator then
 		local Mouse = Player:GetMouse()
 		for i = 1, #MouseEvents do
-			Mouse[MouseEvents[i]]:Connect(function(...)
-				fakeRemote:FireServer(MouseEvents[i], ...)
+			Mouse[MouseEvents[i]]:Connect(function(Key)
+				fakeRemote:FireServer({
+					Action = "Mouse",
+					Event = MouseEvents[i],
+					Key = Key
+				})
 			end)
 		end
 	else
-		local Mouse = {}
 		for i = 1, #MouseEvents do
 			Mouse[MouseEvents[i]] = fakeEvent()
 		end
 		function Mouse.GetMouse()
 			return Mouse
 		end
+		fakeRemote.OnClientEvent:Connect(function(args)
+			if args.Action == "Mouse" then
+				Mouse[args.Event]:Fire(args.Key)
+				return
+			end
+		end)
 	end
+	local Services = {
+		Players = setmetatable({
+			LocalPlayer = 
+		}, newObject(Players))
+	}
+	local game = setmetatable(Services, newObject(real.game))
+	function game:GetService(service)
+		return Services[service] or real.game:GetService(service)
+	end
+	function game:service(...) return self:GetService(...) end
 	
+	local env = {
+		game = game
+	}
+	for i,v in next, env do
+		getfenv(2)[i] = v
+	end
 end
